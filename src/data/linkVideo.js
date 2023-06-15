69df77b6b501beb79c16c6bb3d5592f9e2ea04b4
Fra <135766845+JungleKiosk@users.noreply.github.com>
2023-06-15 13:01:46 +0200

    fix error with link to video

--- src/data/linkVideo.js
+++ src/data/linkVideo.js
@@ -3,25 +3,25 @@ export const links = [
         img: "zoning_aoi.png",
         title: 'Zoning',
         desc: "Using a raster DTM to calculate terrain elevation, slope, and exposure. From these parameters a suitability map will be created. Identifying the most suitable areas for cultivation.",
-        video: "https://youtu.be/g-XKZ76nAas",
+        video: "https://www.youtube.com/watch?v=g-XKZ76nAas&list=PLR8J_sq3CKNdzW_hmx5DbGNq0VYvr7nn5",
     },
     {
         img: "countur.png",
         title: ' Contour lines Python',
         desc: "Contour lines from the DTM are calculated using the \"Countour Raster\" function. From the Python shell, a code is used to extract the main isolines.",
-        video: "https://youtu.be/II1RBk-aOdw",
+        video: "https://www.youtube.com/watch?v=LGTUD4GpGSI&list=PLR8J_sq3CKNdzW_hmx5DbGNq0VYvr7nn5&index=2",
     },
     {
         img: "mmqgis.png",
         title: ' Import zoning in Google Earth',
         desc: "Import a shape file in KML format into Google Earth Pro, so as to explore geographical data and their characteristics in an interactive three-dimensional environment.",
-        video: "https://youtu.be/yPG2pi0Jf2A",
+        video: "https://www.youtube.com/watch?v=XywgV_05M_o&list=PLR8J_sq3CKNdzW_hmx5DbGNq0VYvr7nn5&index=3",
     },
     {
         img: "ctr.png",
         title: ' Import WMS',
         desc: "By inserting the URL of interest in the \"Add WMS Layers\" function in QGIS you will be able to view very heavy cartographic data, taking advantage of the access to the remote server.",
-        video: "https://youtu.be/jyreBxH9I48",
+        video: "https://www.youtube.com/watch?v=0eUkL8LVEZE&list=PLR8J_sq3CKNdzW_hmx5DbGNq0VYvr7nn5&index=4",
     }
 ]
 
